3cc6bc71f6eb7083eedc0b91bf593491cb3d149c
pvcvillamanan@gmail.com <paola@Paola.localdomain>
2026-01-20 18:37:52 +0000

    feat:implement post route and sorting clients list

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router";
 function Header ()  {
     return (
         <>
-            <nav>
-                    <Link to={"/clients"}>Clients</Link>
-                    <Link to={"/new-client"}>New Client</Link>
-            </nav>
-            
+            <header className="header">
+                <nav className="nav">
+                        <Link to={"/clients"}>Clients</Link>
+                        <Link to={"/new-client"}>New Client</Link>
+                </nav>
             <h1>Client Tracker</h1>
-                
+            </header>   
         </>
     );
 }
