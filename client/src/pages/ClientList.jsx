e66ccb95a4de7eda8219ecd0b2d930fd35bef50f
pvcvillamanan@gmail.com <paola@Paola.localdomain>
2026-01-25 17:16:19 +0000

    update render url

--- client/src/pages/ClientList.jsx
+++ client/src/pages/ClientList.jsx
@@ -10,7 +10,7 @@ function ClientList()   {
 
     useEffect(() => {
         async function fetchClients()   {
-            const response = await fetch("http://localhost:8080/clients");
+            const response = await fetch("https://client-tracker-1-juzl.onrender.com/clients");
             const data = await response.json();
         
             setClients(data)      
